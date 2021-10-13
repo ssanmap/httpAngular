9f151d2b849e8c8b47f4274d9831449c6dcca2c4
sebastian san martin <sebastian.smp21@gmail.com>
2021-10-13 11:55:55 -0300

    mergeando con nav

---
+++ src/app/services/categories.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Category } from '../models/category.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  private apiUrl = 'https://damp-spire-59848.herokuapp.com/api/categories';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll(limit?: number, offset?: number) {
+    let params = new HttpParams();
+    if (limit && offset) {
+      params = params.set('limit', limit)
+      params = params.set('offset', offset)
+    }
+    return this.http.get<Category[]>(this.apiUrl, {params})
+  }
+}
